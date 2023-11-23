46b2ad39255fa6a10a19f40f0ad4dc332208872e
savyobrenner <savyobrenner@gmail.com>
2023-11-23 08:02:37 -0300

    feat: remove padding from borders

--- Sources/ExyteMediaPicker/Views/Pages/MediaPicker/AlbumSelectionView.swift
+++ Sources/ExyteMediaPicker/Views/Pages/MediaPicker/AlbumSelectionView.swift
@@ -95,11 +95,11 @@ public struct ModeSwitcher: View {
             RoundedRectangle(cornerRadius: 8)
             .stroke(Color(uiColor: UIColor(red: 0.949, green: 0.698, blue: 0.188, alpha: 1)), lineWidth: 1)
             .background(Color.clear)
+            .ignoresSafeArea()
         )
         .onAppear {
-//            UISegmentedControl.appearance().selectedSegmentTintColor = UIColor(red: 0.949, green: 0.698, blue: 0.188, alpha: 1)
             UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor(red: 0.08, green: 0.08, blue: 0.08, alpha: 1)], for: .selected)
-            UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor(red: 0.95, green: 0.7, blue: 0.19, alpha: 1)], for: .normal)
+            UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor(red: 0.949, green: 0.698, blue: 0.188, alpha: 1)], for: .normal)
             UISegmentedControl.appearance().backgroundColor = .clear
         }
     }
